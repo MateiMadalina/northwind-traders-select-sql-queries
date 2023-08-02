bab75f313f4e805ba02978aae7a21199063c9fec
MateiMadalina <116349352+MateiMadalina@users.noreply.github.com>
2023-08-02 17:02:08 +0300

    task-7

--- task-7.sql
+++ task-7.sql
@@ -1,1 +1,5 @@
 -- YOUR SOLUTION HERE
+SELECT ship_country FROM orders
+WHERE EXTRACT(YEAR FROM shipped_date) = '1997'
+   OR EXTRACT(YEAR FROM shipped_date) = '1998'
+    AND (ship_country ILIKE 's%');
